d752afae3ae93c2fd752e6ac7a16a385378b5a5b
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-10-16 14:30:54 -0400

    unicode

---
+++ Unicode/Unicode.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Unicode
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAnswer = new System.Windows.Forms.ListBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstAnswer
+            // 
+            this.lstAnswer.FormattingEnabled = true;
+            this.lstAnswer.Location = new System.Drawing.Point(32, 81);
+            this.lstAnswer.Name = "lstAnswer";
+            this.lstAnswer.Size = new System.Drawing.Size(216, 147);
+            this.lstAnswer.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(99, 31);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lstAnswer);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstAnswer;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ Unicode/Unicode.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by: Ryan St. Louis
+ * Created on: 16-10-2015
+ * Created for: ICS3U
+ * Daily Assignment – unit# 3-11
+ * This program displays ASCII
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unicode
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // Varibles
+            const int maxNumber = 91;
+            // Process
+            for (int loopCounter = 65; loopCounter != maxNumber; loopCounter++)
+            {
+                this.lstAnswer.Items.Add(Char.ConvertFromUtf32(loopCounter) + " --> " + loopCounter);
+            }
+        }
+    }
+}
